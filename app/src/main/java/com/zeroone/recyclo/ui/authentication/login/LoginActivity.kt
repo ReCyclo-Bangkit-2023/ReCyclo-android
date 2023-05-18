0ac372f6fb743801feee483ccead117d6b728891
mohammad firmansyah <firmansmoh842@gmail.com>
2023-05-18 21:07:20 +0700

    Initial commit

---
+++ app/src/main/java/com/zeroone/recyclo/ui/authentication/login/LoginActivity.kt
@@ -0,0 +1,25 @@
+package com.zeroone.recyclo.ui.authentication.login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zeroone.recyclo.MainActivity
+import com.zeroone.recyclo.R
+import com.zeroone.recyclo.databinding.ActivityLoginBinding
+import com.zeroone.recyclo.ui.authentication.register.RegisterActivity
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        binding.registerBtn.setOnClickListener{
+            startActivity(Intent(this@LoginActivity,RegisterActivity::class.java))
+        }
+    }
+}

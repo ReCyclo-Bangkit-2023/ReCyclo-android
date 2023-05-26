834d81fc6edbf88a078b73e2f13e28a7bf2fb921
mohammad firmansyah <firmansmoh842@gmail.com>
2023-05-26 14:02:22 +0700

    add longlist page

--- app/src/main/java/com/zeroone/recyclo/ui/home/HomeActivity.kt
+++ app/src/main/java/com/zeroone/recyclo/ui/home/HomeActivity.kt
@@ -1,14 +1,27 @@
 package com.zeroone.recyclo.ui.home
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.zeroone.recyclo.R
+import com.zeroone.recyclo.databinding.ActivityHomeBinding
+import com.zeroone.recyclo.databinding.ActivityLoginBinding
+import com.zeroone.recyclo.ui.longlist.LonglistActivity
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityHomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
 
         supportActionBar?.hide()
+
+        binding.waste.setOnClickListener {
+            startActivity(Intent(this@HomeActivity,LonglistActivity::class.java))
+        }
+
     }
 }

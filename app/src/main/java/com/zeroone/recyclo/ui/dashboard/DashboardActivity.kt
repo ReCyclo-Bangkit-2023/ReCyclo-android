20cfaec84d08d2f9c61734751c97e0985a0f8595
mohammad firmansyah <firmansmoh842@gmail.com>
2023-05-27 10:20:41 +0700

    add card for dashboard

---
+++ app/src/main/java/com/zeroone/recyclo/ui/dashboard/DashboardActivity.kt
@@ -0,0 +1,12 @@
+package com.zeroone.recyclo.ui.dashboard
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.zeroone.recyclo.R
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}

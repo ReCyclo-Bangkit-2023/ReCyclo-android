44b3d3ed821f6803f5d86f506c6977deb36b33db
mohammad firmansyah <firmansmoh842@gmail.com>
2023-05-28 16:52:58 +0700

    add gallery

--- app/src/main/java/com/zeroone/recyclo/ui/dashboard/goods/add/AddActivity.kt
+++ app/src/main/java/com/zeroone/recyclo/ui/dashboard/goods/add/AddActivity.kt
@@ -1,13 +1,40 @@
 package com.zeroone.recyclo.ui.dashboard.goods.add
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import com.zeroone.recyclo.R
 import com.zeroone.recyclo.databinding.ActivityAddGoodsBinding
+import java.io.File
 
 class AddActivity : AppCompatActivity() {
 
+    private lateinit var img1 : File
     private lateinit var binding : ActivityAddGoodsBinding
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == AppCompatActivity.RESULT_OK) {
+            val selectedImg = result.data?.data as Uri
+            selectedImg.let { uri ->
+                val myFile = com.zeroone.recyclo.utils.Image.uriToFile(uri, this@AddActivity)
+                binding.imageView11.setImageURI(uri)
+                img1 = myFile
+            }
+        }
+    }
+
+    private fun startGallery(id : Int) {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Choose a Picture")
+        launcherIntentGallery.launch(chooser,id)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddGoodsBinding.inflate(layoutInflater)
@@ -15,7 +42,7 @@ class AddActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         binding.img1.setOnClickListener {
-
+            startGallery(1)
         }
 
     }

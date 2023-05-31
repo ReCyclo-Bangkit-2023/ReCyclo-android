303d1b2b2b248ba9b7a65363c4a4f61feb143285
mohammad firmansyah <firmansmoh842@gmail.com>
2023-05-31 15:52:24 +0700

    fix error bottom navigation to be stick on the bottom

--- app/src/main/java/com/zeroone/recyclo/ui/longlist/LonglistActivity.kt
+++ app/src/main/java/com/zeroone/recyclo/ui/longlist/LonglistActivity.kt
@@ -1,14 +1,25 @@
 package com.zeroone.recyclo.ui.longlist
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.zeroone.recyclo.R
+import com.zeroone.recyclo.databinding.ActivityLonglistBinding
+import com.zeroone.recyclo.ui.detail.DetailActivity
 
 class LonglistActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLonglistBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_longlist)
+        binding = ActivityLonglistBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.hide()
+
+        binding.bestSellingProduct1.setOnClickListener {
+            startActivity(Intent(this@LonglistActivity,DetailActivity::class.java))
+        }
+
+
 
     }
 }

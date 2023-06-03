83235ad0ecb684e04692499692cf1bf9f39ad53d
mohammad firmansyah <firmansmoh842@gmail.com>
2023-06-03 22:39:59 +0700

    add auto slider img at DetailActivity

--- app/src/main/java/com/zeroone/recyclo/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/zeroone/recyclo/ui/detail/DetailActivity.kt
@@ -2,12 +2,36 @@ package com.zeroone.recyclo.ui.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.smarteist.autoimageslider.SliderView
 import com.zeroone.recyclo.R
+import com.zeroone.recyclo.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityDetailBinding
+    lateinit var imageUrl: ArrayList<String>
+    lateinit var sliderView: SliderView
+    lateinit var sliderAdapter: SliderAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         supportActionBar?.hide()
+
+        sliderView = findViewById(R.id.imageSlider)
+        imageUrl = ArrayList()
+        imageUrl =
+            (imageUrl + "https://practice.geeksforgeeks.org/_next/image?url=https%3A%2F%2Fmedia.geeksforgeeks.org%2Fimg-practice%2Fbanner%2Fdsa-self-paced-thumbnail.png&w=1920&q=75") as ArrayList<String>
+        imageUrl =
+            (imageUrl + "https://practice.geeksforgeeks.org/_next/image?url=https%3A%2F%2Fmedia.geeksforgeeks.org%2Fimg-practice%2Fbanner%2Fdata-science-live-thumbnail.png&w=1920&q=75") as ArrayList<String>
+        imageUrl =
+            (imageUrl + "https://practice.geeksforgeeks.org/_next/image?url=https%3A%2F%2Fmedia.geeksforgeeks.org%2Fimg-practice%2Fbanner%2Ffull-stack-node-thumbnail.png&w=1920&q=75") as ArrayList<String>
+
+        sliderAdapter = SliderAdapter( imageUrl)
+        sliderView.autoCycleDirection = SliderView.LAYOUT_DIRECTION_LTR
+        sliderView.setSliderAdapter(sliderAdapter)
+        sliderView.scrollTimeInSec = 3
+        sliderView.isAutoCycle = true
+        sliderView.startAutoCycle()
     }
 }

6b0449f2b38a33845d8bef3e72b3f5b0e3f8d0bd
mohammad firmansyah <firmansmoh842@gmail.com>
2023-06-03 23:21:21 +0700

    fix hide support action bar transaction and cart activity

--- app/src/main/java/com/zeroone/recyclo/ui/cart/CartActivity.kt
+++ app/src/main/java/com/zeroone/recyclo/ui/cart/CartActivity.kt
@@ -3,10 +3,16 @@ package com.zeroone.recyclo.ui.cart
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.zeroone.recyclo.R
+import com.zeroone.recyclo.databinding.ActivityCartBinding
 
 class CartActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityCartBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cart)
+        binding = ActivityCartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
     }
 }
--- app/src/main/java/com/zeroone/recyclo/ui/transaction/TransactionActivity.kt
+++ app/src/main/java/com/zeroone/recyclo/ui/transaction/TransactionActivity.kt
@@ -3,10 +3,15 @@ package com.zeroone.recyclo.ui.transaction
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.zeroone.recyclo.R
+import com.zeroone.recyclo.databinding.ActivityTransactionBinding
 
 class TransactionActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityTransactionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_transaction)
+        binding = ActivityTransactionBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
     }
 }

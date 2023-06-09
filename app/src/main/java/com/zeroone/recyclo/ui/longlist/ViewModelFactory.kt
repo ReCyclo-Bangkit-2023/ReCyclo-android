3ea724665f7e3eecf162540e311918120ec086b0
mohammad firmansyah <firmansmoh842@gmail.com>
2023-06-09 18:28:09 +0700

    add model factory

---
+++ app/src/main/java/com/zeroone/recyclo/ui/longlist/ViewModelFactory.kt
@@ -0,0 +1,16 @@
+package com.zeroone.recyclo.ui.longlist
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.zeroone.recyclo.model.SessionPreference
+
+class ViewModelFactory(private val context: Context, private val sessionPreference: SessionPreference) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(LonglistViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return LonglistViewModel(Injection.provideRepository(context,sessionPreference),sessionPreference) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
